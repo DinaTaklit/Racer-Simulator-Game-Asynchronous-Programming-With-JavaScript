3949f06c6062e78967ecffb40f2586700921457f
DinaTaklit <ez_taklit@esi.dz>
2021-05-28 01:36:01 +0100

    update: clientjs handleSelectTrack save the selected track id to the store

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -175,8 +175,8 @@ function handleSelectTrack(target) {
 	// add class selected to current target
 	target.classList.add('selected')
 
-	// TODO - save the selected track id to the store
-	
+	// save the selected track id to the store
+	store.track_id = parseInt(target.id);
 }
 
 function handleAccelerate() {
